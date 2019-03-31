0bc9a4aa02bc87ced806431e1739b927cfb7b97f
danitoth4 <tdaniel9854@gmail.com>
2019-03-31 15:14:10 +0200

    removed some unneccessary stuff

--- src/main/java/model/ModelManager.java
+++ src/main/java/model/ModelManager.java
@@ -12,55 +12,10 @@ public class ModelManager
     //singleton
     private static ModelManager instance = null;
 
-    //just to test this
-    private String[] crops = new String[] {"tomato", "carrot"};
-
-    private ArrayList<Crop> cropData = new ArrayList<>();
-
     private  Garden currentGarden = new Garden(200, 200);
 
     private ModelManager()
     {
-        for(String name : crops)
-        {
-
-            try
-            {
-                URL url = new URL("https://openfarm.cc/api/v1/crops/"+ name + "/");
-                HttpURLConnection con = (HttpURLConnection) url.openConnection();
-                con.setRequestMethod("GET");
-                int status = con.getResponseCode();
-
-                if(status != 200)
-                {
-                    System.out.println("error");
-                }
-
-                BufferedReader in = new BufferedReader(
-                        new InputStreamReader(con.getInputStream()));
-                String inputLine;
-                StringBuffer content = new StringBuffer();
-                while ((inputLine = in.readLine()) != null) {
-                    content.append(inputLine);
-                }
-                in.close();
-                cropData.add(setupCrop(content));
-            }
-            catch (MalformedURLException e)
-            {
-                e.printStackTrace();
-            }
-            catch (ProtocolException e)
-            {
-                e.printStackTrace();
-            }
-            catch (IOException e)
-            {
-                e.printStackTrace();
-            }
-
-        }
-        createXML();
     }
 
     public Garden getCurrentGarden() {
@@ -71,75 +26,11 @@ public class ModelManager
         this.currentGarden = currentGarden;
     }
 
-    private void createXML() {
-        try
-        {
-            JAXBContext jaxbContext = JAXBContext.newInstance(Crop.class);
-            Marshaller marshaller = jaxbContext.createMarshaller();
-            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-            for(Crop c : cropData)
-            {
-                marshaller.marshal(c, new File("C:\\git\\biogarden\\src\\main\\resources\\Models\\" + c.getName() +".xml"));
-            }
-        }
-        catch (JAXBException e )
-        {
-            System.out.println("AU");
-        }
-
-    }
-
-    private Crop setupCrop(StringBuffer content) {
-        JSONTokener tokener = new JSONTokener(content.toString());
-        JSONObject root = new JSONObject(tokener);
-
-        JSONObject data = root.getJSONObject("data");
-        JSONObject attributes = data.getJSONObject("attributes");
-
-        Crop newCrop = new Crop();
-
-
-        newCrop.setName(attributes.getString("name"));
-        newCrop.setBinomialName(attributes.getString("binomial_name"));
-        newCrop.setDescription(attributes.getString("description"));
-        switch(attributes.getString("sun_requirements"))
-        {
-            case "Full Sun":
-                newCrop.setSunRequirement(SunType.FULLSUN);
-                break;
-            case "Partial Sun":
-                newCrop.setSunRequirement(SunType.PARTIALSUN);
-                break;
-            case "Full Shade":
-                newCrop.setSunRequirement(SunType.FULLSHADE);
-                break;
-            default:
-                newCrop.setSunRequirement(SunType.FULLSUN);
-                break;
-        }
-        newCrop.setSowingMethod(attributes.getString("sowing_method"));
-        newCrop.setDiameter((float)attributes.getDouble("spread"));
-        newCrop.setRowSpacing((float)attributes.getDouble("row_spacing"));
-        newCrop.setHeight((float)attributes.getDouble("height"));
-
-        return newCrop;
-    }
-
     public static ModelManager getInstance()
     {
         if(instance == null)
             instance = new ModelManager();
         return instance;
-    }
-
-    public final Crop getData(String name)
-    {
-        for(Crop c : cropData)
-        {
-            if(c.getName().toLowerCase().equals(name.toLowerCase()))
-                return c;
-        }
-        return null;
     }
 
 }
